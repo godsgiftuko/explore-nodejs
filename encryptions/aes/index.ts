6ed0acc29fb25420b03b0c87ddb22f96eae79402
God'sgift Uko <godsgiftuko@gmail.com>
2023-10-16 00:45:40 +0100

    Added AES Encryption

---
+++ encryptions/aes/index.ts
@@ -0,0 +1,58 @@
+import * as crypto from 'crypto';
+
+const config = {
+  privateKey: 'PRIVATE_KEY' // Enter private key
+}
+
+export interface IEncryption {
+  encrypt: (strToEncrypt: string) => string | null;
+  decrypt: (strToDecrypt: string) => string | null;
+}
+
+export class AesEncryption implements IEncryption {
+  private privateKey: string;
+
+  constructor(privateKey: string) {
+    this.privateKey = privateKey;
+  }
+
+  encrypt(strToEncrypt): string | null {
+    try {
+      const iv = Buffer.alloc(16, 0);
+      const key = crypto.pbkdf2Sync(this.privateKey, iv, 65536, 32, 'sha256');
+
+      const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+      let encrypted = cipher.update(strToEncrypt, 'utf8', 'base64');
+      encrypted += cipher.final('base64');
+
+      return encrypted;
+    } catch (e) {
+      console.log('Error while encrypting:', e.toString());
+    }
+    return null;
+  }
+
+  decrypt(strToDecrypt): string | null {
+    try {
+      const iv = Buffer.alloc(16, 0);
+      const key = crypto.pbkdf2Sync(this.privateKey, iv, 65536, 32, 'sha256');
+
+      const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+      let decrypted = decipher.update(strToDecrypt, 'base64', 'utf8');
+      decrypted += decipher.final('utf8');
+
+      return decrypted;
+    } catch (e) {
+      console.log('Error while decrypting:', e.toString());
+    }
+    return null;
+  }
+}
+
+// Test
+const encryption = new AesEncryption(config.privateKey);
+const encrypted = encryption.encrypt('hello world!');
+const decrypted = encryption.decrypt(encrypted);
+
+console.log('%encrypted', encrypted);
+console.log('%decrypted', decrypted);
